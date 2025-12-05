663fd7060896ba398dbd966ee536880871828325
craftr <nawedarshad25@gmail.com>
2025-12-05 07:15:07 +0530

    Cors Fix

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,31 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS FIX FOR WEB + MOBILE
   app.enableCors({
-    origin: process.env.CORS_ALLOWED_ORIGINS?.split(',') ?? '*',
+    origin: (origin, callback) => {
+      // Allow mobile apps (origin = null)
+      if (!origin) return callback(null, true);
+
+      // Allow all local and production domains
+      const allowed = [
+        'http://localhost:3000',
+        'http://192.168.1.5:3000',
+        'http://167.71.229.252:3000',
+      ];
+
+      if (allowed.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(null, false);
+    },
     credentials: true,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: '*',
   });
 
-  await app.listen(process.env.PORT || 4000);
+  // MOST IMPORTANT FOR VPS !! 🔥
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 bootstrap();
